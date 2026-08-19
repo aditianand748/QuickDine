c923e5c8fc3e2c8bc6717b557f124a036fd01170
Aditi <aditianand748@gmail.com>
2026-08-19 08:08:49 +0530

    added api for authentication

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/authController.js";
+import { getMe, loginUser, registerUser } from "../controllers/authController.js";
+import { protect } from "../middlewares/auth.js";
 
 const authRouter = Router()
 
 authRouter.post("/register", registerUser)
+authRouter.post("/login", loginUser)
+authRouter.get("/me", protect, getMe)
+
+export default authRouter;
