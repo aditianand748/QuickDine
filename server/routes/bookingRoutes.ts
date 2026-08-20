17a463502ad6627d9611f4772aaa4ee4f8b67227
Aditi <aditianand748@gmail.com>
2026-08-20 11:09:13 +0530

    api for booking created

--- server/routes/bookingRoutes.ts
+++ server/routes/bookingRoutes.ts
@@ -1,3 +1,11 @@
 import { Router } from "express";
+import { cancelBooking, createBooking, getMyBookings } from "../controllers/bookingController.js";
+import { protect } from "../middlewares/auth.js";
 
 const bookingRouter = Router();
+
+bookingRouter.post("/", protect, createBooking)
+bookingRouter.get("/my", protect, getMyBookings)
+bookingRouter.put("/:id/cancel", protect, cancelBooking)
+
+export default bookingRouter;
